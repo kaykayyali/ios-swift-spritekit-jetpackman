c6565bfb5c017777dda4c0ea8675090801c83fac
Kaysser Kayyali <kaysser@kaykayyali.com>
2015-04-21 14:11:44 -0400

    Initial Commit

---
+++ Jet Pack ManTests/Jet_Pack_ManTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jet_Pack_ManTests.swift
+//  Jet Pack ManTests
+//
+//  Created by Kaysser Kayyali on 4/21/15.
+//  Copyright (c) 2015 Kaysser Kayyali. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Jet_Pack_ManTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
