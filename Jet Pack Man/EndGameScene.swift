530a2f0894ff157eebf3d436c6f59b799f5167e8
Kaysser Kayyali <kaykayyali@gmail.com>
2015-08-11 10:52:04 -0400

    Working Version

---
+++ Jet Pack Man/EndGameScene.swift
@@ -0,0 +1,9 @@
+//
+//  EndGameScene.swift
+//  Jet Pack Man
+//
+//  Created by Kaysser Kayyali on 4/24/15.
+//  Copyright (c) 2015 Kaysser Kayyali. All rights reserved.
+//
+
+import Foundation
---
+++ Jet Pack Man/JetPackMan.swift
@@ -0,0 +1,13 @@
+//
+//  JetPackMan.swift
+//  Jet Pack Man
+//
+//  Created by Kaysser Kayyali on 4/21/15.
+//  Copyright (c) 2015 Kaysser Kayyali. All rights reserved.
+//
+
+import UIKit
+
+class JetPackMan: SKNode {
+   
+}
